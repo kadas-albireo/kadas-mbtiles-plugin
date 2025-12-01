e00dc4b0b67956037f4a8a590e202509ca2ac445
Valentin Buira <valentin.buira@gmail.com>
2025-12-01 22:54:07 +0100

    cleanup

--- kadas_mbtiles/kadas_mbtiles_export_dialog.py
+++ kadas_mbtiles/kadas_mbtiles_export_dialog.py
@@ -37,13 +37,9 @@ class KadasMBTilesExportDialog(QDialog, WidgetUi):
 
         self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(False)
 
-        #     self.spinBoxExportScale.setValue(int(iface.mapCanvas().mapSettings().scale()))
 
         self.buttonSelectFile.clicked.connect(self.__selectOutputFile)
-        #     self.checkBoxClear.toggled.connect(self.__updateLocalLayerList)
-        #     self.checkBoxExportScale.toggled.connect(self.spinBoxExportScale.setEnabled)
 
-        #     self.mGroupBoxExtent.toggled.connect(self.__extentToggled)
 
 
 
@@ -53,13 +49,6 @@ class KadasMBTilesExportDialog(QDialog, WidgetUi):
 
         self.mGroupBoxExtent.toggled.connect(self.__extentToggled)
 
-
-    #     self.mRectTool = KadasMapToolSelectRect(iface.mapCanvas())
-    #     self.mRectTool.rectChanged.connect(self.__extentChanged)
-    #     iface.mapCanvas().setMapTool(self.mRectTool)
-
-    #     self.labelCheckIcon.setPixmap(QPixmap(":/images/themes/default/mIconSuccess.svg"))
-    #     self.labelWarnIcon.setPixmap(QPixmap(":/images/themes/default/mIconWarning.svg"))
 
     def outputFile(self):
         return self.lineEditOutputFile.text()
@@ -79,33 +68,6 @@ class KadasMBTilesExportDialog(QDialog, WidgetUi):
     def metatileSize(self):
         return self.metaTileSizeSpinBox.value()
 
-    # def clearOutputFile(self):
-    #     return self.checkBoxClear.isChecked()
-
-    # def selectedLayers(self):
-    #     return self.listWidgetLayers.getSelectedLayers()
-
-    # def buildPyramids(self):
-    #     return self.checkBoxPyramids.isChecked()
-
-    # def rasterExportScale(self):
-    #     return self.spinBoxExportScale.value() if self.checkBoxExportScale.isChecked() else None
-
-    # def filterExtent(self):
-    #     if not self.mGroupBoxExtent.isChecked():
-    #         return None
-
-    #     return self.mRectTool.rect()
-
-    # def filterExtentCrs(self):
-    #     if not self.mGroupBoxExtent.isChecked():
-    #         return None
-
-    #     return self.iface.mapCanvas().mapSettings().destinationCrs()
-
-    # def clear(self):
-    #     self.iface.mapCanvas().unsetMapTool(self.mRectTool)
-    #     self.mRectTool = None
 
     def __selectOutputFile(self):
         lastDir = QSettings().value("/UI/lastImportExportDir", ".")
@@ -129,29 +91,7 @@ class KadasMBTilesExportDialog(QDialog, WidgetUi):
         self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(filename is not None)
         # self.__updateLocalLayerList()
 
-    # def __updateLocalLayerList(self):
-    #     self.listWidgetLayers.updateLayerList(self.lineEditOutputFile.text() if not self.checkBoxClear.isChecked() else None)
 
-    # def __extentToggled(self, checked):
-    #     if checked:
-    #         self.mRectTool.setRect( self.iface.mapCanvas().extent() )
-    #     else:
-    #         self.mRectTool.clear()
-
-    # def __extentChanged(self, extent):
-    #     if extent.isNull():
-    #         self.mLineEditXMin.setText("")
-    #         self.mLineEditYMin.setText("")
-    #         self.mLineEditXMax.setText("")
-    #         self.mLineEditYMax.setText("")
-    #     else:
-    #         decs = 0
-    #         if self.iface.mapCanvas().mapSettings().mapUnits() == Qgis.DistanceUnit.Degrees:
-    #             decs = 3
-    #         self.mLineEditXMin.setText(f"{extent.xMinimum(): {decs}f}")
-    #         self.mLineEditYMin.setText(f"{extent.yMinimum(): {decs}f}")
-    #         self.mLineEditXMax.setText(f"{extent.xMaximum(): {decs}f}")
-    #         self.mLineEditYMax.setText(f"{extent.yMaximum(): {decs}f}")
 
     def accept(self):
 
